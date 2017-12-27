c63b452c6455fe8296a71c3a50159666578008ee
Jack Greenberg <theProgrammerJack@gmail.com>
2017-12-27 13:52:04 -0500

    Added check to see if too much time has passed since the request was sent. This should filter out requests sent while the heroku server is waking from sleep.

--- main.dart
+++ main.dart
@@ -55,7 +55,12 @@ Future handlePost(HttpRequest req) async {
 }
 
 void handleEvent(Map<String, dynamic> json, HttpRequest req) async {
-	if (!json.containsKey("token") || json["token"] != Platform.environment["SLACK_VERIFICATION_TOKEN"]) {
+	if (new DateTime.now().millisecondsSinceEpoch - req.headers.date.millisecondsSinceEpoch > 2500) {
+		req.response
+			..statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+			..write("Server was waking from sleep, please retry.")
+			..close();
+	} else if (!json.containsKey("token") || json["token"] != Platform.environment["SLACK_VERIFICATION_TOKEN"]) {
 		req.response
 			..statusCode = HttpStatus.UNAUTHORIZED
 			..write("Invalid verification token")
